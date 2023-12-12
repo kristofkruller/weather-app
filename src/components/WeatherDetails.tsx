144f1f053fdaa702d3521c29b5f349c07dec7cc3
kristofkruller <kristof.kruller@gmail.com>
2023-12-12 19:04:59 +0100

    minor style fix

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -14,7 +14,7 @@ const WeatherDetails = ({weather}: WeatherDetailsProps) => {
     <p className='weather-det'><CiTempHigh className={'det-icon'}/>{weather.main.temp}°C</p>
     <p className='weather-det'><FiSunrise className={'det-icon'}/>{getSunTime(weather.sys.sunrise)}</p>
     <p className='weather-det'><FiSunset className={'det-icon'}/>{getSunTime(weather.sys.sunset)}</p>
-    <p className='weather-det'><FaWind className={'det-icon'}/>{weather.wind.speed} m/s <GiDirectionSigns className={'det-icon'}/>{getWindDirection(weather.wind.deg)}</p>
+    <p className='weather-det'><FaWind className={'det-icon'}/>{weather.wind.speed} m/s <GiDirectionSigns className={'det-icon ml-1'}/>{getWindDirection(weather.wind.deg)}</p>
     <p className='weather-det mt-5'>Feels like {weather.main.feels_like}°C</p>
     <p className='weather-det'><WiHumidity className={'det-icon'}/>{getHumidityValue(weather.main.humidity)}</p>
     <p className='weather-det'><MdVisibility className={'det-icon'}/>{getVisibilityValue(weather.visibility)}</p>
